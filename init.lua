9b83d6c2079521953e90209a3d404a49c81e309c
ronrob-lu <ronrob@emojized.com>
2026-07-05 17:55:27 -0400

    agi new second

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 -- Initialize random seed
-math.randomseed(os.time())
+-- math.randomseed is not necessary as Minetest seeds math.random already
 
 -- Helper function to generate a random texture
 local function get_random_texture()
@@ -16,42 +16,42 @@ minetest.register_entity("nystreets:npc", {
         textures = {"character.png"},
         makes_footstep_sound = true,
         stepheight = 1.1,
-        automatic_rotate = math.pi * 0.5,
+        
     },
-
+    
     on_activate = function(self, staticdata, dtime_s)
         -- Set random texture and nametag
         self.object:set_properties({
             textures = {get_random_texture()},
             nametag = "", -- No nametag
         })
-
+        
         -- Start walking animation (assuming standard Minetest character animation frames 168-187 for walking)
         self.object:set_animation({x=168, y=187}, 30, 0)
-
+        
         -- Set initial direction
         local yaw = math.random() * math.pi * 2
         self.object:set_yaw(yaw)
-
+        
         local dir = minetest.yaw_to_dir(yaw)
         self.object:set_velocity({x=dir.x * 3, y=0, z=dir.z * 3})
         self.object:set_acceleration({x=0, y=-9.81, z=0})
-
+        
         self.timer = 0
         self.stuck_timer = 0
         self.last_pos = self.object:get_pos()
     end,
-
+    
     on_step = function(self, dtime)
         self.timer = self.timer + dtime
-
+        
         -- Every 0.5 seconds, check if stuck
         if self.timer > 0.5 then
             self.timer = 0
-
+            
             local pos = self.object:get_pos()
             if not pos then return end
-
+            
             -- Check if despawn is needed (too far from any player)
             local nearest_player = false
             for _, player in ipairs(minetest.get_connected_players()) do
@@ -61,27 +61,27 @@ minetest.register_entity("nystreets:npc", {
                     break
                 end
             end
-
+            
             if not nearest_player then
                 self.object:remove()
                 return
             end
-
+            
             -- Check for movement
             if vector.distance(pos, self.last_pos) < 0.1 then
                 self.stuck_timer = self.stuck_timer + 1
-
+                
                 if self.stuck_timer > 2 then
                     -- Very stuck, change direction to try to find a way out
                     local yaw = self.object:get_yaw()
                     yaw = yaw + (math.random() * math.pi - math.pi/2)
                     self.object:set_yaw(yaw)
-
+                    
                     local dir = minetest.yaw_to_dir(yaw)
                     -- Jump
                     local vel = self.object:get_velocity()
                     self.object:set_velocity({x=dir.x * 3, y=vel.y + 4.5, z=dir.z * 3})
-
+                    
                     self.stuck_timer = 0
                 else
                     -- Slightly stuck, maybe just need to jump
@@ -93,9 +93,9 @@ minetest.register_entity("nystreets:npc", {
             else
                 self.stuck_timer = 0
             end
-
+            
             self.last_pos = pos
-
+            
             -- Re-apply velocity based on yaw to ensure straight lines
             local yaw = self.object:get_yaw()
             local dir = minetest.yaw_to_dir(yaw)
@@ -103,7 +103,7 @@ minetest.register_entity("nystreets:npc", {
             self.object:set_velocity({x=dir.x * 3, y=vel.y, z=dir.z * 3})
         end
     end,
-
+    
     -- When hit, it takes damage (default behavior works fine)
 })
 -- Global Spawner
@@ -112,11 +112,11 @@ minetest.register_globalstep(function(dtime)
     spawn_timer = spawn_timer + dtime
     if spawn_timer < 5 then return end
     spawn_timer = 0
-
+    
     for _, player in ipairs(minetest.get_connected_players()) do
         local pos = player:get_pos()
         if pos then
-
+        
         -- Count NPCs around player
         local objects = minetest.get_objects_inside_radius(pos, 100)
         local npc_count = 0
@@ -126,7 +126,7 @@ minetest.register_globalstep(function(dtime)
                 npc_count = npc_count + 1
             end
         end
-
+        
         -- Spawn if below 100
         if npc_count < 100 then
             -- Spawn up to 10 at a time to reduce lag spikes
@@ -135,22 +135,22 @@ minetest.register_globalstep(function(dtime)
                 -- Spawn 70-80 blocks away
                 local dist = math.random(70, 80)
                 local angle = math.random() * math.pi * 2
-
+                
                 local spawn_x = pos.x + math.cos(angle) * dist
                 local spawn_z = pos.z + math.sin(angle) * dist
-
+                
                 -- Find valid height
                 local spawn_y = pos.y + 20 -- start above
                 local found_ground = false
-
+                
                 for y = pos.y + 20, pos.y - 20, -1 do
                     local node_pos = {x = spawn_x, y = y, z = spawn_z}
                     local node = minetest.get_node(node_pos)
-
+                    
                     if node.name ~= "air" and minetest.registered_nodes[node.name] and minetest.registered_nodes[node.name].walkable then
                         local above1 = minetest.get_node({x = spawn_x, y = y + 1, z = spawn_z})
                         local above2 = minetest.get_node({x = spawn_x, y = y + 2, z = spawn_z})
-
+                        
                         if above1.name == "air" and above2.name == "air" then
                             spawn_y = y + 1
                             found_ground = true
@@ -158,11 +158,11 @@ minetest.register_globalstep(function(dtime)
                         end
                     end
                 end
-
+                
                 if found_ground then
                     local spawn_pos = {x = spawn_x, y = spawn_y, z = spawn_z}
                     local obj = minetest.add_entity(spawn_pos, "nystreets:npc")
-
+                    
                     if obj then
                         -- Face towards the opposite side of the player's viewable area
                         -- Generally just direct them across the area, so angle + pi
@@ -172,7 +172,7 @@ minetest.register_globalstep(function(dtime)
                 end
             end
         end
-
+        
         end -- close if pos then
     end
 end)
